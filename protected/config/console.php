e7bdac15dabed91bdcb156b56617b28ad5fba22e
Roenke <roenke54@gmail.com>
2014-08-12 11:48:01 +0700

    fix

--- protected/config/console.php
+++ protected/config/console.php
@@ -11,19 +11,19 @@ return array(
 
 	// application components
 	'components'=>array(
-		'db'=>array(
-			'connectionString' => 'sqlite:'.dirname(__FILE__).'/../data/testdrive.db',
-		),
+	//	'db'=>array(
+	//		'connectionString' => 'sqlite:'.dirname(__FILE__).'/../data/testdrive.db',
+	//	),
 		// uncomment the following to use a MySQL database
-		/*
+		
 		'db'=>array(
-			'connectionString' => 'mysql:host=localhost;dbname=testdrive',
+			'connectionString' => 'pgsql:host=localhost;dbname=gis_test',
 			'emulatePrepare' => true,
-			'username' => 'root',
-			'password' => '',
-			'charset' => 'utf8',
+			'username' => 'admin',
+			'password' => 'admin',
+			'charset' => 'utf8'
 		),
-		*/
+		
 		'log'=>array(
 			'class'=>'CLogRouter',
 			'routes'=>array(
@@ -33,5 +33,5 @@ return array(
 				),
 			),
 		),
-	),
+	)	
 );
