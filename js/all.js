f25d8950534ec197677193702dcccd667b4839be
Symin Ivan <iwan.svr@yandex.ru>
2014-08-13 16:19:14 +0700

    оптимизация JSON

--- js/all.js
+++ js/all.js
@@ -1,9 +1,16 @@
 $(document).ready(function() {
+    $('.close').click(function(){
+        $('#test_div').hide();
+    });
+    $('#test_div').hide();
     $('#formS').submit(function() {
+        $('#test_div').show();
+
         $.getJSON('http://localhost/api/search', {}, function(data) {
             $('#baza').html('').append(data.name + '<br/>').append(data.address);
         });
         return false;
     })
+
 });
 
--- protected/views/site/index.php
+++ protected/views/site/index.php
@@ -10,9 +10,11 @@ $this->pageTitle=Yii::app()->name;
                 <form method="POST">
                     <img src="<?php echo Yii::app()->request->baseUrl; ?>/images/logo-2gis.png">
 
-                    <input type="text"  name="search"       id="search"  autofocus  placeholder="Введите организацию либо адрес"   maxlength="500"  >
+                    <input type="text"  name="search"
+                           id="search"  autofocus  placeholder="Введите организацию либо адрес"
+                           maxlength="500"  >
 
-                    <a href="javascript:hidediv('test_div');">  <input type="button" value="Поиск" id="submit"  class="skrPokazat">  </a>
+                     <input type="submit" value="Поиск" id="submit">
 
                 </form>
             </div>
@@ -21,13 +23,13 @@ $this->pageTitle=Yii::app()->name;
 
 
 
-            <div id="test_div" style="display:none;">
+            <div id="test_div" style="display:block;">
 
                 <div id="carta" class="carta">
                     <div id="org">
                         <h2 class="font2">Организации</h2>
 
-                        <a href="javascript:closer('test_div');">   <img src="<?php echo Yii::app()->request->baseUrl; ?>/images/cross.png" class="close"></a>
+                           <img src="<?php echo Yii::app()->request->baseUrl; ?>/images/cross.png" class="close">
                     </div>
                     <div id="baza">
                         <div id="minCarta"></div>
@@ -36,19 +38,8 @@ $this->pageTitle=Yii::app()->name;
                 </div>
             </div>
 
-            <script>
-                function hidediv(get_id){
-                    div=document.getElementById(get_id);
-                    if(div.style.display !== "block") div.style.display = "block";
-
-                }
-            </script>
 
 
-<script>
-    function closer(get_id){
-        div=document.getElementById(get_id);
-        if(div.style.display !== "none") div.style.display = "none";
 
-    }
-</script>
+
+
