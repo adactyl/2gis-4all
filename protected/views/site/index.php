269ad99276a70f5dfa9eae7a2f74d39fc9e693ce
Symin Ivan <iwan.svr@yandex.ru>
2014-08-13 15:25:49 +0700

    доработка панели

--- protected/views/site/index.php
+++ protected/views/site/index.php
@@ -12,47 +12,43 @@ $this->pageTitle=Yii::app()->name;
 
                     <input type="text"  name="search"       id="search"  autofocus  placeholder="Введите организацию либо адрес"   maxlength="500"  >
 
-                    <input type="submit" value="Поиск" id="submit"  class="skrPokazat">
+                    <a href="javascript:hidediv('test_div');">  <input type="button" value="Поиск" id="submit"  class="skrPokazat">  </a>
 
                 </form>
             </div>
 
-            <div id="map"  >
+            <div id="map"> </div>
 
 
 
-
-
-            </div>
-
-            <a href="javascript:hidediv('test_div');"> <input type="button" value="Скрыть\Показать" id="submit1"> </a>
-
-            <div id="test_div" style="display:block;">
+            <div id="test_div" style="display:none;">
 
                 <div id="carta" class="carta">
                     <div id="org">
                         <h2 class="font2">Организации</h2>
+
+                        <a href="javascript:closer('test_div');">   <img src="<?php echo Yii::app()->request->baseUrl; ?>/images/cross.png" class="close"></a>
                     </div>
                     <div id="baza">
-                        <div id="minCarta">
-
-
-                        </div>
-
+                        <div id="minCarta"></div>
                     </div>
 
                 </div>
-
-
-
-
-
-
             </div>
+
             <script>
                 function hidediv(get_id){
                     div=document.getElementById(get_id);
                     if(div.style.display !== "block") div.style.display = "block";
-                    else div.style.display = "none"
+
                 }
             </script>
+
+
+<script>
+    function closer(get_id){
+        div=document.getElementById(get_id);
+        if(div.style.display !== "none") div.style.display = "none";
+
+    }
+</script>
