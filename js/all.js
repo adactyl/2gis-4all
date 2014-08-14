1b43587417e07eaccc79b8941a2761166fca40d4
Roenke <roenke54@gmail.com>
2014-08-14 18:16:54 +0700

    clicable little card, it is temporary solution while we have not api for get-mote-information-by-id

--- js/all.js
+++ js/all.js
@@ -10,9 +10,39 @@ $(document).ready(function() {
 
 });
 */
+var onLittleCardRender = function(element, index, array){
+    $('#cardField').append('<div class="littleCard">'
+        + '<div class="littleCard_icon"></div>'
+        + '<div class="littleCard_name">' + element.name + '</div>'
+        + '<div class="littleCard_address">' + element.address + '</div>'
+        + '<div class="cheat">' + element.id + '</div></div>');
+
+}
+
+var onLittleCardClick = function(){
+    if($('body').find('#cardOpen').length === 0){
+        $('body').append('<div id="cardOpen">New. id = ' + $(this).find('.cheat').html() + '</div>');
+    }
+    else{
+        $('#cardOpen').html('').append('Old. id = ' + $(this).find('.cheat').html());
+    }
+    /*var apiUrl = 'http://localhost/api/fullInfoById',
+        methodParams = {'id': $(this).find('.cheat').html()};
+    $.getJSON(apiUrl, methodParams, function(data){
+        console.log('data was receive');
+
+    });*/
+}
+
+var onSuccessJson = function(data){
+    $('#cardField').html('').append(data.length + ' records was found' + '<br/>');
+    data.forEach(onLittleCardRender);
+    $('.littleCard').click(onLittleCardClick);
+}
+
 
 $(document).ready(function() {
-    //Bind onClick to
+
     $('.close').click(function(){
         //Hide search result on click
         $('#card').hide();
@@ -24,22 +54,9 @@ $(document).ready(function() {
     $('#searchForm').submit(function() {
         //Show search result
         $('#card').show();
-        //Work with api
-        $.getJSON(
-            'http://localhost/api/search',
-            {
-                line: $('#searchInput').val()
-            },
-            //Successful function
-            function(data) {
-                $('#cardField').html('').append(data.length + ' records was found' + '<br/>');
-                data.forEach(function(element, index, array){
-                    $('#cardField').append('<div class="littleCard">'
-                        + 'id = ' + element.id + '<br/>'
-                        + 'name = ' + element.name + '<br/>'
-                        + 'address = ' + element.address  + '</div>');
-                });
-            });
+        var methodParams = {line:$('#searchInput').val()},
+            apiUrl = 'http://localhost/api/search';
+        $.getJSON(apiUrl, methodParams, onSuccessJson);
         return false;
     })
 });
