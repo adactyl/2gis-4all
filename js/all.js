5120ee6cc088ab8a0189798f21b12277a038e4e9
Roenke <roenke54@gmail.com>
2014-08-15 15:52:44 +0700

    reset all markers after mew search

--- js/all.js
+++ js/all.js
@@ -10,15 +10,18 @@ $(document).ready(function() {
 
 });
 */
+/* Отрисовка маленькой карточки, вызывается через for Each */
 var onLittleCardRender = function(element, index, array){
     $('#cardField').append('<div class="littleCard">'
         + '<div class="littleCard_icon"></div>'
         + '<div class="littleCard_name">' + element.name + '</div>'
         + '<div class="littleCard_address">' + element.address + '</div>'
         + '<div class="cheat">' + element.id + '</div></div>');
-    DG.marker([element.latitude, element.longitude]).addTo(map).bindPopup(element.name);
+    DG.marker([element.latitude, element.longitude]).addTo(markers).bindPopup(element.name);
 }
 
+/* ToDo: пока что костыль, когда будет готов backend сделать отправку json и рендер большой карточки */
+/* Обработчик при нажатии на маленькую карточку */
 var onLittleCardClick = function(){
     if($('body').find('#cardOpen').length === 0){
         $('body').append('<div id="cardOpen">New. id = ' + $(this).find('.cheat').html() + '</div>');
@@ -28,45 +31,56 @@ var onLittleCardClick = function(){
     }
     else{
         $('#cardOpen').html('').append('Old. id = ' + $(this).find('.cheat').html());
-
     }
-    /*var apiUrl = 'http://localhost/api/fullInfoById',
-        methodParams = {'id': $(this).find('.cheat').html()};
-    $.getJSON(apiUrl, methodParams, function(data){
-        console.log('data was receive');
 
-    });*/
+    /*var apiUrl = 'http://localhost/api/fullInfoById',
+     methodParams = {'id': $(this).find('.cheat').html()};
+     $.getJSON(apiUrl, methodParams, function(data){
+     console.log('data was receive');*/
 }
 
 var onSuccessJson = function(data){
-    //ToDo: Отображать после организаций серым шрифтом. Переделать костыль
-    $('#cardField').html('');
     $('.fontAmount').html('');
     $('.font2').append('<span class="fontAmount">  ' + data.length  + '</span>');
 
+    /* Удалим старые маркеры с карты */
+    markers.removeFrom(map);
+    /* Сбросим маркеры */
+    markers = DG.featureGroup();
+    /* Удалим старые карточки */
+    $('#cardField').html('');
+    /* Рендерим карты, формируем маркеры */
     data.forEach(onLittleCardRender);
+    /* Выведем новые маркеры на карту */
+    markers.addTo(map);
+    /* К новым карточкам добавим обработчик к onClick */
     $('.littleCard').click(onLittleCardClick);
+
 }
 
 
 $(document).ready(function() {
-    //ToDo: рефакторинг
+
+    /* Изначально окно поиска спрятано */
+    $('#card').hide();
+
     $('.close').click(function(){
-        //Hide search result on click
+        /* при нажатии на крестик спрячем окно поиска */
         $('#card').hide();
     });
-    //Hide search result on document load
-    $('#card').hide();
+
 
     //onClick button "Search"
     $('#searchForm').submit(function() {
-        //Show search result
+        /* Убрать лишние проблемы из строки поиска и сравнить её с пустой строкой */
         var searchLine = $('#searchInput').val().trim();
         if(searchLine === ''){
             $('#searchInput').val('');
             return false;
         }
+        /* При успешной валидации показать окно поиска */
         $('#card').show();
+        /* получить json */
         var methodParams = {line: searchLine},
             apiUrl = 'http://localhost/api/search';
         $.getJSON(apiUrl, methodParams, onSuccessJson);
