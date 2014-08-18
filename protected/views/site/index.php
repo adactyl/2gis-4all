a9a2c45f8d1be392f272cf2cc5919525bcd65bce
Денис <d.vorontsov.nsk@yandex.ru>
2014-08-18 11:28:32 +0700

    Исправил буковку.

--- protected/views/site/index.php
+++ protected/views/site/index.php
@@ -14,7 +14,7 @@ $this->pageTitle=Yii::app()->name;
                            id="searchInput"  autofocus  placeholder="Введите организацию либо адрес"
                            autocomplete="off"
                            maxlength="500"  pattern="[0-9-A-Za-zА-Яа-яЁё\s]{3,}"
-                           title="Минимальной число символов: 3. Разрешены только буквы и цифры.">
+                           title="Минимальное число символов: 3. Разрешены только буквы и цифры.">
 
                      <input type="submit" value="Поиск" id="searchSubmit">
 
